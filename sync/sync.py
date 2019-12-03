0822163db50d52cea82148e18f251786da3cd7eb
sgalal <32255369+chromezh@users.noreply.github.com>
2019-12-03 23:32:16 +0800

    Add columns in database

--- sync/sync.py
+++ sync/sync.py
@@ -44,8 +44,7 @@ CREATE TEMP TABLE 廣韻小韻1
 , '韻1' TEXT NOT NULL
 , '韻' TEXT NOT NULL
 , '反切' TEXT
-);
-''')
+);''')
 
 data_small_rhyme_1 = pandas.read_csv('sync/SieuxYonh.txt', sep=' ', header=None, usecols=[0, 1, 2, 3, 4, 5], names=['SmallRhymeId', 'SmallRhyme', 'Initial', 'Rhyme1', 'Rhyme', 'Fanqie'])
 cur.executemany('INSERT INTO 廣韻小韻1 VALUES (?, ?, ?, ?, ?, ?)', zip(data_small_rhyme_1['SmallRhymeId'], data_small_rhyme_1['SmallRhyme'], data_small_rhyme_1['Initial'], data_small_rhyme_1['Rhyme1'], data_small_rhyme_1['Rhyme'], data_small_rhyme_1['Fanqie']))
@@ -58,8 +57,7 @@ CREATE TEMP TABLE 廣韻小韻2
 , '韻1' TEXT NOT NULL
 , '等' INTEGER NOT NULL
 , '開合' TEXT NOT NULL
-);
-''')
+);''')
 
 data_small_rhyme_2 = pandas.read_csv('sync/YonhMux.txt', sep=' ', na_filter=False, usecols=['#韻母', '等', '呼'])
 cur.executemany('INSERT INTO 廣韻小韻2 VALUES (?, ?, ?, ?)', zip(repeat(None), data_small_rhyme_2['#韻母'], data_small_rhyme_2['等'], data_small_rhyme_2['呼']))
@@ -74,8 +72,7 @@ CREATE TEMP TABLE 廣韻小韻3
 , '白一平轉寫' TEXT NOT NULL
 , '推導中州音' TEXT NOT NULL
 , '推導普通話' TEXT
-);
-''')
+);''')
 
 data_extd_small_rhyme = pandas.read_csv('sync/PrengQim.txt', sep=' ', keep_default_na=False, na_values=[''])  # https://stackoverflow.com/a/27173640
 data_extd_small_rhyme2 = pandas.read_csv('sync/Dauh.txt', sep=' ', na_filter=False, usecols=['推導中州音', '推導普通話'])
@@ -107,8 +104,7 @@ AND 等 <= 4
 AND LENGTH(上字) = 1
 AND LENGTH(下字) = 1
 )
-);
-''')
+);''')
 
 cur.execute('''
 INSERT INTO 廣韻小韻
@@ -118,8 +114,7 @@ substr(反切, 1, 1) AS 上字, substr(反切, 2) AS 下字,
 FROM 廣韻小韻1 INNER JOIN 廣韻小韻2
 USING (韻1)
 INNER JOIN 廣韻小韻3
-USING (小韻號);
-''')
+USING (小韻號);''')
 
 ## Emplace 廣韻字頭
 
@@ -130,8 +125,7 @@ CREATE TABLE 廣韻字頭
 , '字頭' TEXT NOT NULL
 , '解釋' TEXT NOT NULL
 , PRIMARY KEY (小韻號, 小韻內字序)
-);
-''')
+);''')
 
 data_char_entity = pandas.read_csv('sync/Dzih.txt', sep=' ', na_filter=False, header=None, names=['Name', 'SmallRhymeId', 'NumInSmallRhyme', 'Explanation'])
 cur.executemany('INSERT INTO 廣韻字頭 VALUES (?, ?, ?, ?)', zip(data_char_entity['SmallRhymeId'], data_char_entity['NumInSmallRhyme'], data_char_entity['Name'], data_char_entity['Explanation']))
@@ -152,31 +146,45 @@ cur.executemany('INSERT INTO 廣韻字頭 VALUES (?, ?, ?, ?)', zip(data_char_en
 
 cur.execute(f'''
 CREATE VIEW 廣韻小韻全 AS
-SELECT 小韻號, 小韻, 韻,
+SELECT 小韻號, 小韻,
+小韻號 || 小韻 || '小韻' AS 小韻全名,
+母 || 開合 || 等漢字 || 韻賅上去入 || 聲 AS 音韻地位,
+韻,
 CASE 韻 {韻到韻賅上去SQL} END AS 韻賅上去,
 韻賅上去入,
 CASE 韻賅上去入 {韻賅上去入到攝SQL} END AS 攝,
 CASE 母 {母到母號SQL} END AS 母號,
-母, 開合, 等, 上字, 下字,
+母, 開合, 等, 等漢字, 聲, 上字, 下字,
 古韻羅馬字, 有女羅馬字, 白一平轉寫, 推導中州音, 推導普通話
 FROM (SELECT 小韻號, 小韻, 韻,
 CASE 韻 {韻到韻賅上去入SQL} END AS 韻賅上去入,
-母, 開合, 等, 上字, 下字,
+母, 開合, 等,
+CASE 等
+WHEN 1 THEN '一'
+WHEN 2 THEN '二'
+WHEN 3 THEN '三'
+ELSE '四'
+END AS 等漢字,
+CASE
+WHEN 小韻號 <= 1156 THEN '平'
+WHEN 小韻號 <= 2091 THEN '上'
+WHEN 小韻號 <= 3182 THEN '去'
+WHEN 小韻號 <= 3874 THEN '入'
+END AS 聲,
+上字, 下字,
 古韻羅馬字, 有女羅馬字, 白一平轉寫, 推導中州音, 推導普通話
-FROM 廣韻小韻);
-''')
+FROM 廣韻小韻);''')
 
 cur.execute('''
 CREATE VIEW 廣韻字頭全 AS
-SELECT 字頭號, 字頭, 解釋, 小韻號, 小韻, 小韻內字序, 韻, 韻賅上去,
-韻賅上去入, 攝, 母號, 母, 開合, 等, 上字, 下字,
+SELECT 字頭號, 字頭, 解釋, 小韻號, 小韻, 小韻全名, 音韻地位, 小韻內字序, 韻, 韻賅上去,
+韻賅上去入, 攝, 母號, 母, 開合, 等, 等漢字, 聲, 上字, 下字,
 古韻羅馬字, 有女羅馬字, 白一平轉寫, 推導中州音, 推導普通話
 FROM (SELECT row_number() OVER (ORDER BY 小韻號, 小韻內字序) AS 字頭號,
 小韻號, 小韻內字序, 字頭, 解釋
 FROM 廣韻字頭)
 INNER JOIN 廣韻小韻全
-USING (小韻號);
-''')
+USING (小韻號);''')
 
 cur.close()
 conn.commit()
