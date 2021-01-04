a3abeddb43955b9b277d76e6442c6ad40febe7fa
sgalal <sgalal.me@outlook.com>
2021-01-04 20:04:18 +0800

    Use poem guangyun data

--- README.md
+++ README.md
@@ -4,13 +4,9 @@
 
 關聯項目：《切韻》音系 JavaScript 函式庫 \([nk2028/qieyun-js](https://github.com/nk2028/qieyun-js)\)。
 
-## 介紹
+## 資料來源
 
-目前，使用電子資料瞭解《切韻》音系的方法主要有三類。一是查看 PDF 格式的影印古籍，二是使用[廣韻全字表](https://zhuanlan.zhihu.com/p/20430939)，三是使用工具網站或軟件，如[韻典網](https://ytenx.org/)、古音小鏡與漢字古今中外讀音查詢 APP。
-
-而若要深入地探索《切韻》音系，還需要對資料進行編程，探索其中隱藏的關係。[《切韻》音系 SQLite 資料庫](https://github.com/nk2028/qieyun-sqlite)不但能實現上述三類方法的功能，還能使用 SQL 語言構建複雜的查詢，並透過編程語言執行更加複雜的操作，達到探索《切韻》音系的目的。
-
-另外，資料庫中的資料與韻典網資料同步。如果韻典網資料有修正，只需要簡單地運行腳本，就可以生成新版本的《切韻》音系 SQLite 資料庫。
+20210104 版本及以後的資料來自[廣韻字音表 (poem 版, 20170209)](https://zhuanlan.zhihu.com/p/20430939)，並做了一定修改；之前的版本資料來自[韻典網](https://ytenx.org/)。
 
 ## 用法
 
@@ -18,7 +14,7 @@
 
 推薦使用 [DB Browser for SQLite](https://sqlitebrowser.org/) 開啓本資料庫。
 
-![Screenshot of DB Browser for SQLite showing the qieyun-sqlite database](screenshot.png)
+![擷圖](screenshot.png)
 
 ## Examples
 
@@ -28,11 +24,10 @@ Examples on Google Colab:
 - [廣韻韻等與開合配合表](https://colab.research.google.com/drive/1VDJJ2N4jjZZ4FsAK_bGPS5m8mIUKzhfM)
 - [廣韻的例外](https://colab.research.google.com/drive/1hmCivFJ2ZWDm8b9Oyk34g-VTFYkd7BJf)
 
-## Build
+## 構建
 
-See [build script](.github/workflows/build.yml).
+參見 [建置腳本]](.github/workflows/build.yml).
 
 ## License
 
-- Data: Public domain.
-- Source code: MIT license.
+Public domain.
---
+++ build.py
@@ -0,0 +1,144 @@
+from itertools import repeat
+import sqlite3
+
+conn = sqlite3.connect('qieyun.sqlite3')
+cur = conn.cursor()
+
+def 生成號SQL(s):
+	return '\n'.join(f"WHEN '{c}' THEN {i}" for i, c in enumerate(s, 1))
+
+母號SQL = 生成號SQL('幫滂並明端透定泥來知徹澄孃精清從心邪莊初崇生俟章昌常書船日見溪羣疑影曉匣云以')
+韻號SQL = 生成號SQL('東冬鍾江支脂之微魚虞模齊祭泰佳皆夬灰咍廢眞臻文欣元魂痕寒刪山仙先蕭宵肴豪歌麻陽唐庚耕清青蒸登尤侯幽侵覃談鹽添咸銜嚴凡')
+
+攝順序 = [
+	('通', '東冬鍾'),
+	('江', '江'),
+	('止', '支脂之微'),
+	('遇', '魚虞模'),
+	('蟹', '齊佳皆灰咍祭泰夬廢'),
+	('臻', '眞諄臻文欣元魂痕'),
+	('山', '寒桓刪山先仙'),
+	('效', '蕭宵肴豪'),
+	('果', '歌戈'),
+	('假', '麻'),
+	('宕', '唐陽'),
+	('梗', '庚耕清青'),
+	('曾', '登蒸'),
+	('流', '侯尤幽'),
+	('深', '侵'),
+	('咸', '覃談鹽添咸銜嚴凡'),
+]
+
+組列表 = [
+	('幫', '幫滂並明'),
+	('端', '端透定泥'),
+	('知', '知徹澄孃'),
+	('精', '精清從心邪'),
+	('莊', '莊初崇生俟'),
+	('章', '章昌常書船'),
+	('見', '見溪羣疑'),
+	('影', '影曉匣云'),
+]
+
+攝號SQL = '\n'.join(f"""WHEN 韻 IN ({','.join(f"'{韻}'" for 韻 in 韻們)}) THEN {攝號}""" for 攝號, (_, 韻們) in enumerate(攝順序, 1))
+攝SQL = '\n'.join(f"""WHEN 韻 IN ({','.join(f"'{韻}'" for 韻 in 韻們)}) THEN '{攝}'""" for 攝, 韻們 in 攝順序)
+組SQL = '\n'.join(f"""WHEN 母 IN ({','.join(f"'{母}'" for 母 in 母們)}) THEN '{組}'""" for 組, 母們 in 組列表)
+
+## 小韻
+
+cur.execute('''
+CREATE TABLE '小韻'
+( '小韻號' INTEGER PRIMARY KEY
+, '母' TEXT NOT NULL CHECK (length(母) = 1)
+, '呼' TEXT NOT NULL CHECK (呼 IN ('開', '合'))
+, '等數字' INTEGER NOT NULL CHECK (等數字 >= 1 AND 等數字 <= 4)
+, '重紐' TEXT CHECK (重紐 IN ('A', 'B'))
+, '韻' TEXT NOT NULL CHECK (length(韻) = 1)
+, '聲' TEXT NOT NULL CHECK (length(韻) = 1)
+, '上字' TEXT CHECK (length(上字) = 1)
+, '下字' TEXT CHECK (length(下字) = 1)
+);''')
+
+def 小韻資料():
+	with open('小韻表.csv') as f:
+		for line in f:
+			小韻號, 母, 呼, 等, 重紐, 韻, 聲, 反切 = line.rstrip('\n').split(',')
+			小韻號 = int(小韻號)
+			等數字 = '〇一二三四'.index(等)
+			重紐 = None if 重紐 == '' else 重紐
+			上字 = None if 反切 == '' else 反切[0]
+			下字 = None if 反切 == '' else 反切[1]
+			yield 小韻號, 母, 呼, 等數字, 重紐, 韻, 聲, 上字, 下字
+
+cur.executemany('INSERT INTO 小韻 VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)', 小韻資料())
+
+## 字頭
+
+cur.execute('''
+CREATE TABLE '字頭'
+( '字頭號' INTEGER PRIMARY KEY
+, '小韻號' INTEGER NOT NULL REFERENCES '小韻'
+, '字頭' TEXT NOT NULL
+, '解釋' TEXT NOT NULL
+);''')
+
+def 字頭資料():
+	with open('字頭表.csv') as f:
+		for line in f:
+			小韻號, 字頭, 釋義 = line.rstrip('\n').split(',')
+			字頭號 = None
+			小韻號 = int(小韻號)
+			yield 字頭號, 小韻號, 字頭, 釋義
+
+cur.executemany('INSERT INTO 字頭 VALUES (?, ?, ?, ?)', 字頭資料())
+
+# Extra
+
+cur.execute(f'''
+CREATE VIEW '小韻全' AS
+SELECT 小韻號,
+母 || 呼 ||
+CASE 等數字
+WHEN 1 THEN '一'
+WHEN 2 THEN '二'
+WHEN 3 THEN '三'
+WHEN 4 THEN '四'
+END
+|| ifnull(重紐, '') || 韻 || 聲 AS 音韻描述,
+母,
+呼,
+CASE 等數字
+WHEN 1 THEN '一'
+WHEN 2 THEN '二'
+WHEN 3 THEN '三'
+WHEN 4 THEN '四'
+END AS 等,
+重紐,
+韻,
+聲,
+CASE {組SQL} END AS 組,
+CASE {攝SQL} END AS 攝,
+CASE 母 {母號SQL} END AS 母號,
+CASE {攝號SQL} END AS 攝號,
+等數字,
+CASE 韻 {韻號SQL} END AS 韻號,
+上字, 下字
+FROM 小韻;''')
+
+cur.execute('''
+CREATE VIEW '字頭全' AS
+SELECT 字頭號, 字頭, 音韻描述,
+母, 呼, 等, 重紐, 韻, 聲,
+組, 攝,
+小韻號,
+母號, 攝號, 等數字, 韻號,
+上字, 下字,
+解釋
+FROM 字頭
+INNER JOIN 小韻全
+USING (小韻號);''')
+
+cur.close()
+conn.commit()
+conn.execute('VACUUM')
+conn.close()
